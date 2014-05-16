ee423c1125a9d22bd3f66bea5a6d8d725bc51cac
Julien <xxx@xxx>
2014-05-16 15:45:44 +0200

    first commit

---
+++ src/Fraction.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fraction.cpp
+ *
+ *  Created on: 16 mai 2014
+ *      Author: xxx
+ */
+
+#include "Fraction.hpp"
+
+Fraction::Fraction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Fraction::~Fraction() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Fraction.hpp
@@ -0,0 +1,17 @@
+/*
+ * Fraction.hpp
+ *
+ *  Created on: 16 mai 2014
+ *      Author: xxx
+ */
+
+#ifndef FRACTION_HPP_
+#define FRACTION_HPP_
+
+class Fraction {
+public:
+	Fraction();
+	virtual ~Fraction();
+};
+
+#endif /* FRACTION_HPP_ */
