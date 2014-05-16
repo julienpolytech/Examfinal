f1c7d29e69cf9852021093af279af164da12e100
Julien <xxx@xxx>
2014-05-16 16:46:18 +0200

    commit

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -7,12 +7,62 @@
 
 #include "Fraction.hpp"
 
+#include <string>
+#include <sstream>
+
 Fraction::Fraction() {
 	// TODO Auto-generated constructor stub
 
 }
 
+Fraction::Fraction(int n,int d){
+}
+
 Fraction::~Fraction() {
 	// TODO Auto-generated destructor stub
 }
+
+
+const int Fraction::getn(){
+    return numerateur;
+}
+
+const int Fraction::getd(){
+    return denominateur;
+}
+
+void Fraction::setn(const int n){
+    numerateur = n;
+}
+
+void Fraction::setd(const int d){
+    denominateur = d;
+}
+
+string Fraction::toString(const int nb)
+{
+   std::ostringstream oss;
+   oss << nb;
+   return oss.str();
+}
+
+string Fraction::Afficher()
+{
+	string toto = "" + toString(this->numerateur) + "/" + toString(this->denominateur);
+
+	return toto;
+}
+
+void Fraction::addition(int add)
+{
+
+}
+void Fraction::addition(const Fraction&)
+{
+
+}
+
+
+
+
 
--- src/Fraction.hpp
+++ src/Fraction.hpp
@@ -8,10 +8,33 @@
 #ifndef FRACTION_HPP_
 #define FRACTION_HPP_
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Fraction {
 public:
 	Fraction();
+	Fraction(int n, int d);
 	virtual ~Fraction();
+	void setn(const int n);
+	void setd(const int d);
+	const int getn();
+	const int getd();
+	string Afficher();
+	string toString(const int nb);
+	void addition(int add);
+	void addition(const Fraction&);
+
+
+private:
+	int numerateur;
+	int denominateur;
 };
+
+
+
+
 
 #endif /* FRACTION_HPP_ */
